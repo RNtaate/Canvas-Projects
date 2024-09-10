a4a97d9fda2e2426bb1a1298e1631825b465f4a2
RNtaate <royhyde14@gmail.com>
2024-09-10 13:18:34 +0300

    Install webpack-dev-server and add script to use dev
    Add configurations for the dev-sever.

--- clickFireworks/webpack.config.js
+++ clickFireworks/webpack.config.js
@@ -11,6 +11,16 @@ module.exports = {
     filename: '[name][contenthash].js',
     clean: true,
   },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
   module: {
     rules: [
       {
