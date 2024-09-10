7f342fd1adf451d19337ac5072a7833215f00503
RNtaate <royhyde14@gmail.com>
2024-09-10 18:31:24 +0300

    Update the event listener to remove the rate multiplier

--- clickFireworks/src/index.js
+++ clickFireworks/src/index.js
@@ -10,18 +10,15 @@ canvas.height = innerHeight;
 const particlesNumber = 20;
 const particles = [];
 const PARTICLE_RADIUS = 10;
-const GRAVITY = 0.05;
+const GRAVITY = 0.02;
 const FRICTION = 0.99;
-const POWER = 2;
-const RATE_MULTIPLIER = 5;
+const POWER = 8;
 
 canvas.addEventListener('click', (event) => {
   for (let i = 1; i <= particlesNumber; i++) {
     let angle = ((Math.PI * 2) / particlesNumber) * i;
-    let xDirection =
-      Math.cos(angle) * Math.floor(Math.random() * RATE_MULTIPLIER + 1) * POWER;
-    let yDirection =
-      Math.sin(angle) * Math.floor(Math.random() * RATE_MULTIPLIER + 1) * POWER;
+    let xDirection = Math.cos(angle) * Math.random() * POWER;
+    let yDirection = Math.sin(angle) * Math.random() * POWER;
     let particle = new Particle(event.clientX, event.clientY, PARTICLE_RADIUS, {
       x: xDirection,
       y: yDirection,
