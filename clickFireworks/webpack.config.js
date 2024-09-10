e5ac99761891febc72601ed098b250afce76e0db
RNtaate <royhyde14@gmail.com>
2024-09-10 12:02:52 +0300

    Add code splitting to the entry configurations which can enable multiple entry files
    Update the filename key in output to dynamically update based on the entry keys.

--- clickFireworks/webpack.config.js
+++ clickFireworks/webpack.config.js
@@ -2,9 +2,11 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: path.resolve(__dirname, 'src/index.js'),
+  entry: {
+    main: path.resolve(__dirname, 'src/index.js'),
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
+    filename: '[name].js',
   },
 };
