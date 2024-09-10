18c3aa14d02fec16179587ec1454dec8a674db30
RNtaate <royhyde14@gmail.com>
2024-09-10 16:49:49 +0300

    Add functionality of drawing multiple particles on the canvas on every click of the canvas.

--- clickFireworks/src/index.js
+++ clickFireworks/src/index.js
@@ -7,6 +7,24 @@ const context = canvas.getContext('2d');
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
-let particle = new Particle(canvas.width / 2, canvas.height / 2, 20);
+const particlesNumber = 20;
+const particles = [];
+const PARTICLE_RADIUS = 10;
 
-particle.draw(context);
+canvas.addEventListener('click', (event) => {
+  for (let i = 1; i <= particlesNumber; i++) {
+    let particle = new Particle(event.clientX, event.clientY, PARTICLE_RADIUS);
+    particles.push(particle);
+  }
+  console.log(particles.length);
+});
+
+function animate() {
+  window.requestAnimationFrame(animate);
+  context.clearRect(0, 0, canvas.width, canvas.height); //clear the canvas on every frame refresh then draw.
+  for (let particle of particles) {
+    particle.draw(context);
+  }
+}
+
+animate();
