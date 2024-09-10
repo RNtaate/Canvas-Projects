caa3a45ec794a93821a185ddaeb689ef79c1c416
RNtaate <royhyde14@gmail.com>
2024-09-10 18:05:05 +0300

    Update the event listener to calculate random angle directions for the various particles, use the extra velocity parameter
    Update the animate function to use the update function, substitute clearRect with fillRect for a nicer effect.

--- clickFireworks/src/index.js
+++ clickFireworks/src/index.js
@@ -7,13 +7,25 @@ const context = canvas.getContext('2d');
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
-const particlesNumber = 20;
+const particlesNumber = 200;
 const particles = [];
 const PARTICLE_RADIUS = 10;
+const GRAVITY = 0.1;
+const FRICTION = 0.99;
+const POWER = 2;
+const RATE_MULTIPLIER = 5;
 
 canvas.addEventListener('click', (event) => {
   for (let i = 1; i <= particlesNumber; i++) {
-    let particle = new Particle(event.clientX, event.clientY, PARTICLE_RADIUS);
+    let angle = ((Math.PI * 2) / particlesNumber) * i;
+    let xDirection =
+      Math.cos(angle) * Math.floor(Math.random() * RATE_MULTIPLIER + 1) * POWER;
+    let yDirection =
+      Math.sin(angle) * Math.floor(Math.random() * RATE_MULTIPLIER + 1) * POWER;
+    let particle = new Particle(event.clientX, event.clientY, PARTICLE_RADIUS, {
+      x: xDirection,
+      y: yDirection,
+    });
     particles.push(particle);
   }
   console.log(particles.length);
@@ -21,9 +33,10 @@ canvas.addEventListener('click', (event) => {
 
 function animate() {
   window.requestAnimationFrame(animate);
-  context.clearRect(0, 0, canvas.width, canvas.height); //clear the canvas on every frame refresh then draw.
+  context.fillStyle = 'rgba(0, 0, 0, 0.1)';
+  context.fillRect(0, 0, canvas.width, canvas.height); //clear the canvas on every frame refresh then draw.
   for (let particle of particles) {
-    particle.draw(context);
+    particle.update(context, GRAVITY, FRICTION);
   }
 }
 
