a6b7b50e48b3d55087b199cda8d107c81d51d8f7
RNtaate <royhyde14@gmail.com>
2024-09-10 18:24:58 +0300

    Implement removal of particles whose alpha is 0

--- clickFireworks/src/index.js
+++ clickFireworks/src/index.js
@@ -7,10 +7,10 @@ const context = canvas.getContext('2d');
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
-const particlesNumber = 200;
+const particlesNumber = 20;
 const particles = [];
 const PARTICLE_RADIUS = 10;
-const GRAVITY = 0.1;
+const GRAVITY = 0.05;
 const FRICTION = 0.99;
 const POWER = 2;
 const RATE_MULTIPLIER = 5;
@@ -35,9 +35,14 @@ function animate() {
   window.requestAnimationFrame(animate);
   context.fillStyle = 'rgba(0, 0, 0, 0.1)';
   context.fillRect(0, 0, canvas.width, canvas.height); //clear the canvas on every frame refresh then draw.
-  for (let particle of particles) {
-    particle.update(context, GRAVITY, FRICTION);
-  }
+
+  particles.forEach((particle, index) => {
+    if (particle.alpha > 0) {
+      particle.update(context, GRAVITY, FRICTION);
+    } else {
+      particles.slice(index, 1);
+    }
+  });
 }
 
 animate();
