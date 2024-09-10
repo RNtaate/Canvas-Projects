24b0712cb0dd818c0de979b9ff9e94bb57ca528a
RNtaate <royhyde14@gmail.com>
2024-09-10 13:50:12 +0300

    Install babel loader, core and preset-env for backwards javascript compatiblility
    Add the configuration to webpack config file for the js files.

--- clickFireworks/webpack.config.js
+++ clickFireworks/webpack.config.js
@@ -28,6 +28,16 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.js$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', { targets: 'defaults' }]],
+          },
+        },
+      },
     ],
   },
   plugins: [
