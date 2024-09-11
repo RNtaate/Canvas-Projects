bd62e71b6f387153a7b7bab52df5757e20c99e76
RNtaate <royhyde14@gmail.com>
2024-09-11 18:49:49 +0300

    Update html design with a canvas
    Try out canvas in index.js file

--- bouncingPlayBalls/src/index.js
+++ bouncingPlayBalls/src/index.js
@@ -1,3 +1,14 @@
 import './styles/index.css';
 
-console.log('Bouncing Play Balls.');
+const canvas = document.getElementById('drawCanvas');
+const context = canvas.getContext('2d');
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+const grad = context.createLinearGradient(0, 0, canvas.width, 0);
+grad.addColorStop(0, 'lightblue');
+grad.addColorStop(1, 'darkblue');
+
+context.fillStyle = grad;
+context.fillRect(0, 0, canvas.width, canvas.height);
