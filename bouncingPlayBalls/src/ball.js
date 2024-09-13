8455b99b272b64392bef2988c1b4240e7b9ffe9a
RNtaate <royhyde14@gmail.com>
2024-09-13 13:58:53 +0300

    Update udpate function by adding energy loss calculation
    Update draw function by adding a stroke design, comment out important but unused code.

--- bouncingPlayBalls/src/ball.js
+++ bouncingPlayBalls/src/ball.js
@@ -3,26 +3,32 @@ class Ball {
     this.x = x;
     this.y = y;
     this.radius = radius;
-    this.color = 'red';
+    this.color = `hsl(${Math.random() * 360}, 50%, 50%)`;
     this.dy = dy;
   }
 
   draw(canvasContext) {
     canvasContext.beginPath();
-    canvasContext.shadowColor = '#333';
-    canvasContext.shadowBlur = 10;
-    canvasContext.shadowOffsetX = 5;
-    canvasContext.shadowOffsetY = 0;
+
+    //--------------------------------------------------------------------------------------------
+    /**Un comment this section if you would like shadows on the balls. Warning: it takes up more computer resources. */
+    // canvasContext.shadowColor = '#333';
+    // canvasContext.shadowBlur = 10;
+    // canvasContext.shadowOffsetX = 5;
+    // canvasContext.shadowOffsetY = 0;
+    /**----------------------------------------------------------------------------------------- */
+
     canvasContext.fillStyle = this.color;
     canvasContext.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
     canvasContext.fill();
+    canvasContext.stroke();
     canvasContext.closePath();
   }
 
-  update(canvas, canvasContext) {
-    let gravity = 1;
+  update(canvas, canvasContext, gravity, energyLoss) {
     if (this.y + this.radius + this.dy >= canvas.height) {
       this.dy = -this.dy;
+      this.dy *= energyLoss;
     } else {
       this.dy += gravity;
     }
