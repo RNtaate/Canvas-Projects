17c8bc43e62f2d9d45a0cf9e4841f075bc92acef
RNtaate <royhyde14@gmail.com>
2024-09-13 13:59:15 +0300

    Add functionality of spawning multiple balls.

--- bouncingPlayBalls/src/index.js
+++ bouncingPlayBalls/src/index.js
@@ -7,12 +7,30 @@ const context = canvas.getContext('2d');
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
-const ball = new Ball(canvas.width / 2, canvas.height / 2, 30, 2);
+const balls = [];
+const numberOfBalls = 20;
+const GRAVITY = 0.5;
+const ENERGY_LOSS = 0.98;
+const radius = 30;
+
+for (let i = 1; i <= numberOfBalls; i++) {
+  let minX = radius;
+  let maxX = canvas.width - radius;
+  let minY = radius;
+  let maxY = canvas.height - radius;
+  let guessedX = Math.floor(Math.random() * (maxX - minX) + minX);
+  let guessedY = Math.floor(Math.random() * (maxY - minY) + minY);
+  console.log(guessedX, guessedY);
+  const ball = new Ball(guessedX, guessedY, radius, 2);
+  balls.push(ball);
+}
 
 function animate() {
   requestAnimationFrame(animate);
   context.clearRect(0, 0, canvas.width, canvas.height);
-  ball.update(canvas, context);
+  balls.forEach((ball) => {
+    ball.update(canvas, context, GRAVITY, ENERGY_LOSS);
+  });
 }
 
 animate();
