1ef579f8afcf38cc4b9627d7b55d874e31632176
RNtaate <royhyde14@gmail.com>
2024-09-18 23:04:42 +0300

    Import Bullet class and implement functionality of creating bullets from the player upon clicking the screen
    Create animate function to redraw drawings on the canvas.

--- Shooting/src/index.js
+++ Shooting/src/index.js
@@ -1,4 +1,5 @@
 import Player from './components/Player';
+import Bullet from './components/Bullet';
 import './styles/index.css';
 
 const canvas = document.querySelector('#gameCanvas');
@@ -7,5 +8,31 @@ const c = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-const player = new Player(canvas.width / 2, canvas.height / 2, 20);
-player.update(c);
+const bullets = [];
+
+const playerRadius = 20;
+const bulletRadius = 4;
+
+const player = new Player(canvas.width / 2, canvas.height / 2, playerRadius);
+
+window.addEventListener('click', (event) => {
+  let x2 = event.clientX;
+  let y2 = event.clientY;
+  let angle = Math.atan2(y2 - canvas.height / 2, x2 - canvas.width / 2);
+  let bullet = new Bullet(canvas.width / 2, canvas.height / 2, bulletRadius, {
+    x: Math.cos(angle),
+    y: Math.sin(angle),
+  });
+  bullets.push(bullet);
+});
+
+function animate() {
+  window.requestAnimationFrame(animate);
+  c.clearRect(0, 0, canvas.width, canvas.height);
+  player.update(c);
+  bullets.forEach((bullet) => {
+    bullet.update(c);
+  });
+}
+
+animate();
