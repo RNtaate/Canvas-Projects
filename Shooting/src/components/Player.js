cbbeee7a9d3fd4a9e0c3a8f35a9c64d0af8a9442
RNtaate <royhyde14@gmail.com>
2024-09-18 23:03:49 +0300

    Update draw function in player class to start and stop drawings.

--- Shooting/src/components/Player.js
+++ Shooting/src/components/Player.js
@@ -7,9 +7,11 @@ class Player {
   }
 
   draw(ctx) {
+    ctx.beginPath();
     ctx.fillStyle = this.color;
     ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
     ctx.fill();
+    ctx.closePath();
   }
 
   update(ctx) {
