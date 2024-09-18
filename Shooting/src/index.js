eef109ddbd9e16e386faef5d34b5933fc1c871a1
RNtaate <royhyde14@gmail.com>
2024-09-18 23:21:00 +0300

    Implement spawning of enemies on the screen.

--- Shooting/src/index.js
+++ Shooting/src/index.js
@@ -1,5 +1,6 @@
 import Player from './components/Player';
 import Bullet from './components/Bullet';
+import Enemy from './components/Enemy';
 import './styles/index.css';
 
 const canvas = document.querySelector('#gameCanvas');
@@ -9,6 +10,7 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 const bullets = [];
+const enemies = [];
 
 const playerRadius = 20;
 const bulletRadius = 4;
@@ -26,12 +28,26 @@ window.addEventListener('click', (event) => {
   bullets.push(bullet);
 });
 
+setInterval(() => {
+  let x = Math.random() * canvas.width;
+  let y = Math.random() * canvas.height;
+  let x2 = canvas.width / 2;
+  let y2 = canvas.height / 2;
+  let angle = Math.atan2(y2 - y, x2 - x);
+
+  let enemy = new Enemy(x, y, 30, { x: Math.cos(angle), y: Math.sin(angle) });
+  enemies.push(enemy);
+}, 1000);
+
 function animate() {
   window.requestAnimationFrame(animate);
   c.clearRect(0, 0, canvas.width, canvas.height);
   player.update(c);
   bullets.forEach((bullet) => {
     bullet.update(c);
+  });
+  enemies.forEach((enemy) => {
+    enemy.update(c);
   });
 }
 
