f4c1a6d18e6c82b8f2bc9294ed1e5eb66da70b02
RNtaate <royhyde14@gmail.com>
2024-09-18 23:03:20 +0300

    Create Bullet class in the components folder

---
+++ Shooting/src/components/Bullet.js
@@ -0,0 +1,25 @@
+class Bullet {
+  constructor(x, y, radius, velocity) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.velocity = velocity;
+    this.color = 'green';
+  }
+
+  draw(ctx) {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.closePath();
+  }
+
+  update(ctx) {
+    this.draw(ctx);
+    this.x += this.velocity.x;
+    this.y += this.velocity.y;
+  }
+}
+
+export default Bullet;
