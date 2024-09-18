4f4a70040a13637ecccd28e6d8fd3535e6db9371
RNtaate <royhyde14@gmail.com>
2024-09-18 21:32:23 +0300

    Create webpack configuration file
    Add various configurations to it.

---
+++ Shooting/webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: path.resolve(__dirname, 'index.js'),
+  },
+  output: {
+    path: path.resolve(__diraname, 'dist'),
+    filename: '[name].js',
+    clean: true,
+    assetModuleFilename: '[name][ext]',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', { targets: 'default' }]],
+          },
+        },
+      },
+      {
+        test: /\.(svg|png|jpe?g|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Shooting Game',
+      filename: 'index.html',
+      template: 'src/template.html',
+    }),
+  ],
+};
