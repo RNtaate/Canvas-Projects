b2fc7b67caa10796f05c4c423d334efd9bd23ecb
RNtaate <royhyde14@gmail.com>
2024-09-18 23:20:33 +0300

    Create Enemy class

---
+++ Shooting/src/components/Enemy.js
@@ -0,0 +1,25 @@
+class Enemy {
+  constructor(x, y, radius, velocity) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.velocity = velocity;
+    this.color = 'blue';
+  }
+
+  draw(ctx) {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.closePath();
+  }
+
+  update(ctx) {
+    this.draw(ctx);
+    this.x += this.velocity.x;
+    this.y += this.velocity.y;
+  }
+}
+
+export default Enemy;
